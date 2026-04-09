cb28cebac2d776225964b8a6ba366734eb4c3234
Your Name <you@example.com>
2026-04-09 21:13:30 +0800

    initial commit

---
+++ TalkingC.cpp
@@ -0,0 +1,23 @@
+/* ill be making another Novel to test my logic understanding using void() 
+With no fancy include only the neccesary and stuff. as usual nut this time
+ill be making using my native Tounge "Filipino" my mother tougne is a bit tricky.
+Anyway, welcome to my chill project that really wants to be readble and then make it
+efficcient later on(dont worry ill make new branch and compare the speed of the two later on)
+
+Why The moon Shines at night |Mindanao| folktale about the tragic story of the first man and woman in
+the world. the man was name dandaw which means the sun, and the woman was name Bulan which mean The moon. Will
+*/
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using std::cout;
+using std::cout;
+using std::string;
+
+int main() {
+
+
+
+    return 0;
+}
